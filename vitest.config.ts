492ca1bec306e47e95ccff1a9eefa3c2bd756e9d
Adrián Clavario <adrianclavario@gmail.com>
2025-11-28 19:35:53 -0300

    modified vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,1 +1,12 @@
-import { defineConfig } from 'vitest/config'; export default defineConfig({ test: { environment: 'node' } });
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    environmentMatchGlobs: [
+      ['apps/frontend/**/*.test.{ts,tsx}', 'jsdom'],
+      ['apps/frontend/**/*.test.{js,jsx}', 'jsdom'],
+    ],
+  },
+});
